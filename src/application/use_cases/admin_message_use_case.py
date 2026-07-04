a184faa1a8cdf8ed746c18c8dca4ba8bfd1e1d31
slonn <vanogalen@yandex.ru>
2026-07-04 17:44:20 +0300

    feat: доделал use case по отправке ответа админа

--- src/application/use_cases/admin_message_use_case.py
+++ src/application/use_cases/admin_message_use_case.py
@@ -4,6 +4,7 @@ from src.application.dto.admin_message import AdminMessageDTO
 from src.domain.mapper import map_admin_message_from_dto
 from src.domain.entities.admin_message import AdminMessage
 from src.domain.values import UserId
+from src.application.interfaces.ws.gateway import WSGatewayAPI
 
 
 class AdminMessageUseCase:
@@ -11,12 +12,16 @@ class AdminMessageUseCase:
             self,
             logger: structlog.BoundLogger,
             redis_storage: RedisStorage,
+            ws_gateway: WSGatewayAPI,
     ):
         self._logger = logger
         self._redis_storage = redis_storage
+        self._ws_gateway = ws_gateway
 
     async def __call__(self, payload: AdminMessageDTO):
         admin_message: AdminMessage = map_admin_message_from_dto(payload)
         user_id: UserId = await self._redis_storage.get(admin_message.message_id)
-        raise NotImplementedError
-        # await self._aio_requests.send()
+        await self._ws_gateway.notify_client(
+            user_id=user_id,
+            text=admin_message.text,
+        )
